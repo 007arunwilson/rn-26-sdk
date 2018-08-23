563b16555f8d1a8f52adb03b26c1e6164449543f
Arun WIlson <cyberinfoscripter@gmail.com>
2018-08-23 17:49:11 +0530

    -react-native-google-signin installed Line edited in gradle file : implementation "com.android.support:appcompat-v7:${safeExtGet('supportLibVersion', '26.1.0')}" to implementation "com.android.support:appcompat-v7:${safeExtGet('supportLibVersion', '27.1.1')}"

--- android/app/src/main/java/com/a26/MainApplication.java
+++ android/app/src/main/java/com/a26/MainApplication.java
@@ -3,6 +3,7 @@ package com.a26;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
             new ReactNativeConfigPackage(),
             new VectorIconsPackage()
       );
